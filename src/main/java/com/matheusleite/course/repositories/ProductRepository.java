ddd96631e78e726e70ec3c13ff49b0d78246ff2c
Matheus Leite <maatheus2710@gmail.com>
2022-06-28 00:57:49 -0300

    Product

---
+++ src/main/java/com/matheusleite/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.matheusleite.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusleite.course.entities.Category;
+import com.matheusleite.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
