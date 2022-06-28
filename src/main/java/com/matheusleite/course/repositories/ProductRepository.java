37773dd36f432f09668e9764c46c2a2b6f15dd68
Matheus Leite <maatheus2710@gmail.com>
2022-06-28 03:15:44 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/matheusleite/course/repositories/ProductRepository.java
+++ src/main/java/com/matheusleite/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.matheusleite.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.matheusleite.course.entities.Category;
 import com.matheusleite.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
